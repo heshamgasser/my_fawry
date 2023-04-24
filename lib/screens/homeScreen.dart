bd42ff016f57e44b76fa30b1f4732f4116ab5e16
Hesham Gasser <heshamgasser32@gmail.com>
2023-04-24 17:32:19 +0300

    remove key

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'Home Screen';
-
-  const HomeScreen({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
